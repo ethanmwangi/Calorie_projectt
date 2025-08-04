7bd327311456c286e7870662196f7206429d5a53
Ethan.Mwangi <ethan.mwangi20067@gmail.com>
2025-08-04 08:56:59 +0300

    form added

--- myproject/myapp/admin.py
+++ myproject/myapp/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import CalorieEntry
 
-# Register your models here.
+admin.site.register(CalorieEntry)
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,5 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import CalorieEntry
+from .forms import CalorieEntryForm
 
 def home(request):
-    return render(request, 'home.html')
-# Create your views here.
+    if request.method == 'POST':
+        form = CalorieEntryForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+    else:
+        form = CalorieEntryForm()
+    entries = CalorieEntry.objects.order_by('-date')
+    return render(request, 'home.html', {'form': form, 'entries': entries})
